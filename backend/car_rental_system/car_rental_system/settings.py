299c35247d54009ea8a69f1e58a99803a893962e
Kumar <k_oskenbay@kbtu.kz>
2025-05-11 00:06:50 +0500

    Deploy 7

--- backend/car_rental_system/car_rental_system/settings.py
+++ backend/car_rental_system/car_rental_system/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', "localhost,127.0.0.1").split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', "localhost,127.0.0.1,drivenow-g8je.onrender.com").split(',')
+
 
 
 # Application definition
